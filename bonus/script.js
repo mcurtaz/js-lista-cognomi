7d97ba28ddfe1dc4c7e836321363535250fd440e
Michele Curtaz <mcurtaz@gmail.com>
2020-06-24 15:51:03 +0200

    Corretta posizione "umana" e iniziato bonus

--- bonus/script.js
+++ bonus/script.js
@@ -12,11 +12,28 @@ listaCognomi.push(cognomeUtente);
 
 console.log(listaCognomi);
 
-// utilizzo sort per riordinare la Lista
 
-listaCognomi.sort();
+
+// BONUS RIORDINAMENTO ALFABETICO LISTA
+
+// il for confronta gli elementi dell'array a due a due e li scambia se necessario. creo una variabile di appoggio che cambia valore a seconda se nel ciclo c'è stato uno scambio tra due elementi. quando il ciclo di scambio percorre tutto l'array senza effettuare nessuno scambio l'array è in ordine alfabetico
+
+
+for (var i = 0; i < listaCognomi.length - 1; i++) {
+  //se localCompare restituisce 1 allora il secondo nome "listaCognome[i+1]" sarebbe in ordine alfabetico prima del primo "listaCognome[i]"
+  if (listaCognomi[i].localeCompare(listaCognomi[i+1]) == 1){
+    var temp = listaCognomi[i+1];
+    listaCognomi[i+1] = listaCognomi[i];
+    listaCognomi[i] = temp;
+  }
+}
+
 
 console.log(listaCognomi);
+
+
+
+
 
 // con un for cerco il cognome aggiunto dall'utente e stampo l'indice corrispondente
 
