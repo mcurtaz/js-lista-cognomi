15580a1e77df627ab6851faeac1a97331a90b235
Michele Curtaz <mcurtaz@gmail.com>
2020-06-24 15:59:01 +0200

    Completato Bonus

--- bonus/script.js
+++ bonus/script.js
@@ -16,22 +16,27 @@ console.log(listaCognomi);
 
 // BONUS RIORDINAMENTO ALFABETICO LISTA
 
-// il for confronta gli elementi dell'array a due a due e li scambia se necessario. creo una variabile di appoggio che cambia valore a seconda se nel ciclo c'è stato uno scambio tra due elementi. quando il ciclo di scambio percorre tutto l'array senza effettuare nessuno scambio l'array è in ordine alfabetico
+// il for confronta gli elementi dell'array a due a due e li scambia se necessario. creo una variabile di appoggio "scambiFiniti" che cambia valore a seconda se nel ciclo c'è stato uno scambio tra due elementi. quando il ciclo di scambio percorre tutto l'array senza effettuare nessuno scambio l'array è in ordine alfabetico
 
+var scambiFiniti = false;
 
-for (var i = 0; i < listaCognomi.length - 1; i++) {
-  //se localCompare restituisce 1 allora il secondo nome "listaCognome[i+1]" sarebbe in ordine alfabetico prima del primo "listaCognome[i]"
-  if (listaCognomi[i].localeCompare(listaCognomi[i+1]) == 1){
-    var temp = listaCognomi[i+1];
-    listaCognomi[i+1] = listaCognomi[i];
-    listaCognomi[i] = temp;
+while (scambiFiniti == false){
+
+  scambiFiniti = true; //All'interno del ciclo do alla variabile valore true. se il for seguente non cambia nessuna posizione si esce dal while. se il for effettua anche solo uno scambio la variabile diventa false e si ricomincia un ciclo di while (e wuindi un altro for che scorre tutto l'array in cerca di cognomi da scambiare)
+
+  for (var i = 0; i < listaCognomi.length - 1; i++) {
+    //se localCompare restituisce 1 allora il secondo nome "listaCognome[i+1]" sarebbe in ordine alfabetico prima del primo "listaCognome[i]"
+    if (listaCognomi[i].localeCompare(listaCognomi[i+1]) == 1){
+      var temp = listaCognomi[i+1]; //creo una variabile temporanea dove salvare un cognome. poi sovrascrivo uno sull'altro. e sul precedente la copia salvata nella variabile temporanea
+      listaCognomi[i+1] = listaCognomi[i];
+      listaCognomi[i] = temp;
+      scambiFiniti = false;
+    }
   }
 }
 
 
 console.log(listaCognomi);
-
-
 
 
 
